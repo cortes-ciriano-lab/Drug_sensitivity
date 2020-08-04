1b9637b83e3f1c3ee386cb5698a5bdbd0f4582d2
aBeatriz <abeatrizg.cunha@gmail.com>
2020-08-04 11:25:27 +0100

    fixed the paths

--- process_datasets.sh
+++ process_datasets.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 rm e.log o.log invalid_smiles.txt
-rm -r data/molecular/run_500/ data/molecular/run_10000/
+#rm -r data/molecular/run_500/ data/molecular/run_10000/
 
 mkdir -p data/
 mkdir -p data/molecular/
-#mkdir -p data/molecular/run_once/
-mkdir -p data/molecular/run_500/
-mkdir -p data/molecular/run_10000/
-#mkdir -p data/molecular/run_once/pkl_files
-#mkdir -p data/molecular/run_once/valid_smiles
-mkdir -p data/molecular/run_500/pkl_files
-mkdir -p data/molecular/run_500/valid_smiles
-mkdir -p data/molecular/run_10000/pkl_files
-mkdir -p data/molecular/run_10000/valid_smiles
-#mkdir -p data/single_cell/
-#mkdir -p data/single_cell/pkl_files
-#mkdir -p data/single_cell/csv_files
-#mkdir -p data/pkl_files/
+mkdir -p data/molecular/run_once/
+#mkdir -p data/molecular/run_500/
+#mkdir -p data/molecular/run_10000/
+mkdir -p data/molecular/run_once/pkl_files
+mkdir -p data/molecular/run_once/valid_smiles
+#mkdir -p data/molecular/run_500/pkl_files
+#mkdir -p data/molecular/run_500/valid_smiles
+#mkdir -p data/molecular/run_10000/pkl_files
+#mkdir -p data/molecular/run_10000/valid_smiles
+mkdir -p data/single_cell/
+mkdir -p data/single_cell/pkl_files
+mkdir -p data/single_cell/csv_files
+mkdir -p data/pkl_files/
 mkdir -p data/prism_pancancer
 
 
